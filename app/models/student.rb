a3cd6f695f17e9f4b9a2c3fa0f4fa5247b38bcc8
Javier Aguilar <jvr.agx@gmail.com>
2020-03-27 09:21:12 -0600

    Update student, professor, professor_student model relationships

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,6 @@
-class Student <ApplicationRecord
-
+class Student < ApplicationRecord
+  validates_presence_of :name, :age, :house
+  belongs_to :professor
+  has_many :professor_students
+  has_many :professors, through: :professor_students
 end
---
+++ spec/features/user_can_see_all_professors_spec.rb
@@ -0,0 +1,24 @@
+# require 'rails_helper'
+#
+# Rspec.describe "As a visitor" do
+#   describe "when I visit /professors"
+#   it "I see a list of professors with their name, age, speciality" do
+#     Professor.create(name: "Severus Snape", age: 45, specialty: "Potions")
+#     Professor.create(name: "Rubus Hagarid", age: 38 , specialty: "Care of Magical Creatures")
+#     Professor.create(name: "Remus Lupin", age: 49 , specialty: "Defense Against The Dark Arts")
+#
+#     visit "/professors"
+#
+#     expect(page).to have_content("Name: Severus Snape")
+#     expect(page).to have_content("Age: 45")
+#     expect(page).to have_content("Specialty: Potions")
+#
+#     expect(page).to have_content("Name: Rubus Hagarid")
+#     expect(page).to have_content("Age: 38")
+#     expect(page).to have_content("Specialty: Care of Magical Creatures")
+#
+#     expect(page).to have_content("Name: Remus Lupin")
+#     expect(page).to have_content("Age: 49")
+#     expect(page).to have_content("Specialty: Defense Against The Dark Arts")
+#   end
+# end
