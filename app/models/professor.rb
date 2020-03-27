83e240b8d0729f2ddffe1a54fe9eff5f32b98788
Javier Aguilar <jvr.agx@gmail.com>
2020-03-27 10:27:23 -0600

    Add average age of students on /professors/:id

--- app/models/professor.rb
+++ app/models/professor.rb
@@ -2,4 +2,10 @@ class Professor < ApplicationRecord
   validates_presence_of :name, :age, :specialty
   has_many :professor_students
   has_many :students, through: :professor_students
+
+  def avg_age_of_students
+    total_age = students.map { |student| student.age }
+    total_age.sum.to_f / students.size
+  end
+
 end
